a41a018276aaf0d7e004e5d66ee9d7954459ba09
VasylHyshchyn <was7yl@gmail.com>
2026-09-21 17:05:53 +0300

    Fixed user-data script

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -2,19 +2,13 @@
 
 sudo yum -y update
 sudo yum -y install docker
-sudo yum -y install git
+
 
 sudo systemctl start docker
 
-mkdir -p /opt/app
-cd /opt/app
-git clone https://github.com/benc-uk/nodejs-demoapp.git
-cd nodejs-demoapp
 sudo docker pull ghcr.io/benc-uk/nodejs-demoapp:latest
 sudo docker tag ghcr.io/benc-uk/nodejs-demoapp:latest nodejs-demoapp:latest
 
-
-sudo docker build -t nodejs-demoapp:latest .
 
 sudo docker run -d \
   --name nodejs-demoapp \
