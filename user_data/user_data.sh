42d82a9c0633cbb32bbc5b21f94c65d85f87d53d
VasylHyshchyn <was7yl@gmail.com>
2026-09-24 20:11:12 +0300

    Removed redundant commands from user-data.

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -6,11 +6,8 @@ sudo yum -y install docker
 
 sudo systemctl start docker
 
-sudo docker pull ghcr.io/benc-uk/nodejs-demoapp:latest
-sudo docker tag ghcr.io/benc-uk/nodejs-demoapp:latest nodejs-demoapp:latest
-
 
 sudo docker run -d \
   --name nodejs-demoapp \
   -p 3000:3000 \
-  nodejs-demoapp:latest
+  ghcr.io/benc-uk/nodejs-demoapp:latest
